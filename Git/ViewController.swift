ff0e13eadd4de75c099591ba20e472432648c07e
tangguangyi <974581882@qq.com>
2015-11-02 21:00:12 +0800

    print
    加入打印语句

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("开源中国")
     }
+    
+    var str = 1
+   
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
